92efa4849f10d78748692758be66839fd39b17d1
olena <elenavmazikina@gmail.com>
2019-08-06 20:00:38 -0400

    asdfasdf

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,6 @@ public class AppTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("Hegit sto World");
+        
     }
 }
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,6 @@
+public class LoginTest {
+    public static void main(String[] args) {
+        System.out.println("line1");
+        System.out.println("line2");
+    }
+}
